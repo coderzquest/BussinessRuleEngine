6d5bd4259afdca228578777b44021c1ffbedac96
Rishi Tiwari <rishtiwari1234@gmail.com>
2021-06-04 17:57:41 +0530

    Added commision rule

--- BussinesRuleEngine.Test/PaymentActivityTest.cs
+++ BussinesRuleEngine.Test/PaymentActivityTest.cs
@@ -19,38 +19,49 @@ namespace BussinesRuleEngine.Test
         public void Payment_ForPhysicalProduct_GeneratePackagingSlip()
         {
             Product product = new PhysicalProduct(1234, "Test Product", 100.50M);
+
             Mock<IGeneratePackingSlip> slipGeneratorRule = new Mock<IGeneratePackingSlip>();
+            Mock<IGenerateCommission> generateCommissionRule = new Mock<IGenerateCommission>();
+
             product.Rules.Add(slipGeneratorRule.Object);
+            product.Rules.Add(generateCommissionRule.Object);
 
             PaymentActivity master = new PaymentActivity(product);
-
             master.Pay();
 
             slipGeneratorRule.Verify(t => t.Exceute(), Times.Once);
+            generateCommissionRule.Verify(t => t.Exceute(), Times.Once);
         }
         [Fact]
         public void Payment_ForBook_GeneratePackagingSlipTwice()
         {
             Product product = new PhysicalProduct(1234, "Test Product", 100.50M);
             Mock<IGeneratePackingSlip> slipGeneratorRule = new Mock<IGeneratePackingSlip>();
+            Mock<IGenerateCommission> generateCommissionRule = new Mock<IGenerateCommission>();
+
             product.Rules.Add(slipGeneratorRule.Object);
             product.Rules.Add(slipGeneratorRule.Object);
+            product.Rules.Add(generateCommissionRule.Object);
+
             PaymentActivity master = new PaymentActivity(product);
 
             master.Pay();
 
             slipGeneratorRule.Verify(t => t.Exceute(), Times.Exactly(2));
+            generateCommissionRule.Verify(t => t.Exceute(), Times.Once);
         }
         [Fact]
         public void Payment_ForActivateMemberShip_ActivateAndSendEmail()
         {
             Product product = new Membership(1234, "RentalMovie", 999);
+
             Mock<ISendEmail> sendMailRule = new Mock<ISendEmail>();
             Mock<IActivateMembership> activateMemberShipRule = new Mock<IActivateMembership>();
-            product.Rules.Add(sendMailRule.Object); 
-            product.Rules.Add(activateMemberShipRule.Object);
-            PaymentActivity master = new PaymentActivity(product);
 
+            product.Rules.Add(sendMailRule.Object);
+            product.Rules.Add(activateMemberShipRule.Object);
+
+            PaymentActivity master = new PaymentActivity(product);
             master.Pay();
 
             sendMailRule.Verify(t => t.Exceute(), Times.Once);
@@ -60,12 +71,14 @@ namespace BussinesRuleEngine.Test
         public void Payment_ForUpgradeMemberShip_UpgradeAndSendEmail()
         {
             Product product = new Membership(1234, "RentalMovie", 999);
+
             Mock<ISendEmail> sendMailRule = new Mock<ISendEmail>();
             Mock<IUpgradeMembership> upgradeMembershipRule = new Mock<IUpgradeMembership>();
+
             product.Rules.Add(sendMailRule.Object);
             product.Rules.Add(upgradeMembershipRule.Object);
-            PaymentActivity master = new PaymentActivity(product);
 
+            PaymentActivity master = new PaymentActivity(product);
             master.Pay();
 
             sendMailRule.Verify(t => t.Exceute(), Times.Once);
---
+++ BussinesRuleEngine/Rules/GenerateCommission.cs
@@ -0,0 +1,21 @@
+﻿using BussinesRuleEngine.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinesRuleEngine.Rules
+{
+    public class GenerateCommission : IGenerateCommission
+    {
+        private readonly Product product;
+
+        public GenerateCommission(Product product) {
+            this.product = product;
+        }
+        public void Exceute()
+        {
+            //GenerateCommision
+        }
+    }
+    public interface IGenerateCommission : IBussinessRule { }
+}
